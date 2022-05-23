62ed8b9bb167456ad720c15e113cc0681ffeee4a
hohad@cambium.co.il <hohad@cambium.co.il>
2022-05-24 00:16:06 +0300

    Docker fix

--- index.mjs
+++ index.mjs
@@ -1,116 +1,151 @@
-// NOT FINISHED YET..
+//  // NOT FINISHED YET..
 
 
-// import http from "http";
-// import ws from "websocket";
-// import redis from "redis";
-// const APPID = process.env.APPID;
-// let connections = [];
-// const WebSocketServer = ws.server;
+import http from "http";
+import ws from "websocket";
+import redis from "redis";
+const APPID = process.env.APPID;
+let connections = [];
+const WebSocketServer = ws.server;
 
-// const subscriber = redis.createClient({
-//   port: 6379,
-//   host: "rds",
-// });
+const subscriber = redis.createClient({
+  port: 6379,
+  host: "rds",
+});
 
-// const publisher = redis.createClient({
-//   port: 6379,
-//   host: "rds",
-// });
+const publisher = redis.createClient({
+  port: 6379,
+  host: "rds",
+});
 
-// subscriber.on("subscribe", function (channel, count) {
-//   console.log(`Server ${APPID} subscribed successfully to livechat`);
-//   publisher.publish("livechat", "a message");
-// });
+subscriber.on("subscribe", function (channel, count) {
+  console.log(`Server ${APPID} subscribed successfully to livechat`);
+  publisher.publish("livechat", "a message");
+});
 
-// subscriber.on("message", function (channel, message) {
-//   try {
-//     //when we receive a message I want t
-//     console.log(
-//       `Server ${APPID} received message in channel ${channel} msg: ${message}`
-//     );
-//     connections.forEach((c) => c.send(APPID + ":" + message));
-//   } catch (ex) {
-//     console.log("ERR::" + ex);
-//   }
-// });
+subscriber.on("message", function (channel, message) {
+  try {
+    //when we receive a message I want t
+    console.log(
+      `Server ${APPID} received message in channel ${channel} msg: ${message}`
+    );
+    connections.forEach((c) => c.send(APPID + ":" + message));
+  } catch (ex) {
+    console.log("ERR::" + ex);
+  }
+});
 
-// subscriber.on("blast", (msg) => {
-//   try {
-//     //when we receive a message I want t
-//     console.log(
-//       `Server ${APPID} received message in channel ${channel} msg: ${msg}`
-//     );
-//     connections.forEach((c) => c.send(APPID + ":" + msg));
-//   } catch (ex) {
-//     console.log("ERR::" + ex);
-//   }
-// });
+subscriber.on("blast", (msg) => {
+  try {
+    //when we receive a message I want t
+    console.log(
+      `Server ${APPID} received message in channel ${channel} msg: ${msg}`
+    );
+    connections.forEach((c) => c.send(APPID + ":" + msg));
+  } catch (ex) {
+    console.log("ERR::" + ex);
+  }
+});
 
-// subscriber.on("spin", (inputwild) => {
-//   let clientsArray = [];
-//   let sentarray = [];
-//   let clientsList = io.sockets.adapter.rooms;
-//   for (let item of clientsList.keys()) clientsArray.push(item);
-//   let choosenRandomUser =
-//     clientsArray[Math.floor(Math.random() * clientsArray.length)];
-//   // If the choosenRandomUser(random function) picked the messages writer, random again.
-//   // If the random picked someone who already picked, random again.
-//   if (
-//     socket.id == choosenRandomUser &&
-//     clientsArray.includes(choosenRandomUser)
-//   ) {
-//     while (
-//       socket.id == choosenRandomUser &&
-//       clientsArray.includes(choosenRandomUser)
-//     ) {
-//       choosenRandomUser =
-//         clientsArray[Math.floor(Math.random() * clientsArray.length)];
-//     }
-//   }
-//   sentarray.push(choosenRandomUser);
-//   sentarray.push(socket.id);
-//   io.to(sentarray).emit("spin", inputwild);
-//   connections.forEach((c) => c.send(APPID + ":" + msg));
-// });
+subscriber.on("spin", (inputwild) => {
+  let clientsArray = [];
+  let sentarray = [];
+  let clientsList = subscriber.sockets.adapter.rooms;
+  for (let item of clientsList.keys()) clientsArray.push(item);
+  let choosenRandomUser =
+    clientsArray[Math.floor(Math.random() * clientsArray.length)];
+  // If the choosenRandomUser(random function) picked the messages writer, random again.
+  // If the random picked someone who already picked, random again.
+  if (
+    subscriber.socket.id == choosenRandomUser &&
+    clientsArray.includes(choosenRandomUser)
+  ) {
+    while (
+      subscriber.socket.id == choosenRandomUser &&
+      clientsArray.includes(choosenRandomUser)
+    ) {
+      choosenRandomUser =
+        clientsArray[Math.floor(Math.random() * clientsArray.length)];
+    }
+  }
+  sentarray.push(choosenRandomUser);
+  sentarray.push(subscriber.socket.id);
+  // io.to(sentarray).emit("spin", inputwild);
+  connections.forEach((c) => c.send("spin", inputwild));
+});
 
-// subscriber.subscribe("livechat");
+subscriber.on("wild", (inputwild, inputwildnum) => {
+  let clientsArray = [];
+  let clientsList = subscriber.sockets.adapter.rooms;
+  let sentarray = [];
+  for (let item of clientsList.keys()) clientsArray.push(item);
 
-// //create a raw http server (this will help us create the TCP which will then pass to the websocket to do the job)
-// const httpserver = http.createServer();
+  // If inputwildnum is bigger then users online, it'll send to everyone the message.
+  if (inputwildnum >= clientsArray.length) {
+    // inputwildnum = clientsArray.length;
+    io.to(clientsArray).emit("wild", inputwild);
+    return;
+  }
+  for (let i = 0; i < inputwildnum; i++) {
+    let choosenRandomUser =
+      clientsArray[Math.floor(Math.random() * clientsArray.length)];
+    // If the choosenRandomUser(random function) picked the messages writer, random again.
+    // If the random picked someone who already picked, random again.
+    if (
+      subscriber.socket.id == choosenRandomUser &&
+      clientsArray.includes(choosenRandomUser)
+    ) {
+      while (
+        subscriber.socket.id == choosenRandomUser &&
+        clientsArray.includes(choosenRandomUser)
+      ) {
+        choosenRandomUser =
+          clientsArray[Math.floor(Math.random() * clientsArray.length)];
+      }
+    }
+    sentarray.push(choosenRandomUser);
+  }
+  sentarray.push(subscriber.socket.id);
+  connections.forEach((c) => c.send("wild", inputwild));
+});
 
-// //pass the httpserver object to the WebSocketServer library to do all the job, this class will override the req/res
-// const websocket = new WebSocketServer({
-//   httpServer: httpserver,
-// });
+subscriber.subscribe("livechat");
 
-// httpserver.listen(8080, () =>
-//   console.log("My server is listening on port 8080")
-// );
+//create a raw http server (this will help us create the TCP which will then pass to the websocket to do the job)
+const httpserver = http.createServer();
 
-// //when a legit websocket request comes listen to it and get the connection .. once you get a connection thats it!
-// websocket.on("request", (request) => {
-//   const con = request.accept(null, request.origin);
-//   con.on("open", () => console.log("opened"));
-//   con.on("close", () => console.log("CLOSED!!!"));
-//   con.on("message", (message) => {
-//     //publish the message to redis
-//     console.log(`${APPID} Received message ${message.utf8Data}`);
-//     publisher.publish("livechat", message.utf8Data);
-//   });
+//pass the httpserver object to the WebSocketServer library to do all the job, this class will override the req/res
+const websocket = new WebSocketServer({
+  httpServer: httpserver,
+});
 
-//   setTimeout(() => con.send(`Connected successfully to server ${APPID}`), 5000);
-//   connections.push(con);
-// });
+httpserver.listen(8080, () =>
+  console.log("My server is listening on port 8080")
+);
 
-// //client code
-// //let ws = new WebSocket("ws://localhost:8080");
-// //ws.onmessage = message => console.log(`Received: ${message.data}`);
-// //ws.send("Hello! I'm client")
+//when a legit websocket request comes listen to it and get the connection .. once you get a connection thats it!
+websocket.on("request", (request) => {
+  const con = request.accept(null, request.origin);
+  con.on("open", () => console.log("opened"));
+  con.on("close", () => console.log("CLOSED!!!"));
+  con.on("message", (message) => {
+    //publish the message to redis
+    console.log(`${APPID} Received message ${message.utf8Data}`);
+    publisher.publish("livechat", message.utf8Data);
+  });
 
-// /*
-//     //code clean up after closing connection
-//     subscriber.unsubscribe();
-//     subscriber.quit();
-//     publisher.quit();
-//     */
+  setTimeout(() => con.send(`Connected successfully to server ${APPID}`), 5000);
+  connections.push(con);
+});
+
+//client code
+//let ws = new WebSocket("ws://localhost:8080");
+//ws.onmessage = message => console.log(`Received: ${message.data}`);
+//ws.send("Hello! I'm client")
+
+/*
+    //code clean up after closing connection
+    subscriber.unsubscribe();
+    subscriber.quit();
+    publisher.quit();
+    */
